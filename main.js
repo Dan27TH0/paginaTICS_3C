23001639e2f3956801328dabb565556860bab2d9
dt094250@gmail.com <dt094250@gmail.com>
2024-06-07 19:32:55 -0600

    Primer Commit

---
+++ main.js
@@ -0,0 +1,42 @@
+/*Programacion
+function saludo (){ //FUNCIONES
+    alert("Saludo desde la funcion");
+}
+
+function otra(){
+    alert("otra funcion");
+}
+*/
+
+var edad=0
+
+function mostrar(){
+    if (edad>=18){
+        alert("Bienvenidos a bodegas alianza...");
+    }
+    else{
+        alert("No tienes edad suficiente para comprar alcohol...");
+    }
+
+    /*switch (key) {
+        case value:
+
+            break;
+
+        default:
+
+            break;
+    }*/
+
+   /* for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }*/
+
+    /*while (condition) {
+        
+    }*/
+}
+function capturar(){
+    edad= prompt("Captura tu edad...");
+}
